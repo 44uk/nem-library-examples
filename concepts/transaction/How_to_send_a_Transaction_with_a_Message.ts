fe2990f60d29a186cd2499a069031fc9428e223c
Aleix Morgadas <aleix602@gmail.com>
2017-07-17 23:30:34 +0200

    errata

--- concepts/transaction/How_to_send_a_Transaction_with_a_Message.ts
+++ concepts/transaction/How_to_send_a_Transaction_with_a_Message.ts
@@ -12,18 +12,4 @@ const transferTransaction: Transaction = TransferTransaction.create(
     new Address("TCFFOM-Q2SBX7-7E2FZC-3VX43Z-TRV4ZN-TXTCGW-BM5J"),
     XEM(2),
     Message.create("a transaction")
-);import {
-    NEMLibrary, NetworkTypes, Address, TransferTransaction, Transaction, TimeWindow,
-    EmptyMessage, MultisigTransaction, PublicAccount, TransactionHttp, Message
-} from "nem-library";
-import {XEM} from "nem-library/dist/src/models/mosaic/XEM";
-
-// Inicializate NEMLibrary for TEST_NET Network
-NEMLibrary.bootstrap(NetworkTypes.TEST_NET);
-
-const transferTransaction: Transaction = TransferTransaction.create(
-    TimeWindow.createWithDeadline(),
-    new Address("TCFFOM-Q2SBX7-7E2FZC-3VX43Z-TRV4ZN-TXTCGW-BM5J"),
-    XEM(2),
-    Message.create("a transaction")
 );
