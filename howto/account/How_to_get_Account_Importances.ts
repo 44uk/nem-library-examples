c2feda39cc004d7f51c49b84f14539afcb646f1e
Aleix Morgadas <aleix602@gmail.com>
2017-07-18 11:17:26 +0200

    CREATED How_to_get_Account_Importances.ts

---
+++ howto/account/How_to_get_Account_Importances.ts
@@ -0,0 +1,10 @@
+import {AccountHttp, Address, NEMLibrary, NetworkTypes} from "nem-library";
+
+NEMLibrary.bootstrap(NetworkTypes.TEST_NET);
+
+const accountHttp = new AccountHttp();
+
+accountHttp.getAccountImportances()
+    .subscribe(x => {
+        console.log(x);
+    });
