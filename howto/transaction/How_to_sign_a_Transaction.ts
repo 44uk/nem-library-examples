7a3026199ac0e47dfeb4dcedf32af6013ef8ad3d
Aleix Morgadas <aleix602@gmail.com>
2017-07-18 11:56:10 +0200

    CREATED How_to_sign_a_Transaction.ts

---
+++ howto/transaction/How_to_sign_a_Transaction.ts
@@ -0,0 +1,26 @@
+import {
+    AccountHttp, NEMLibrary, NetworkTypes, Address, Account, TransferTransaction, TimeWindow,
+    EmptyMessage, MultisigTransaction, PublicAccount, TransactionHttp, XEM
+} from "nem-library";
+declare let process: any;
+
+// Inicializate NEMLibrary for TEST_NET Network
+NEMLibrary.bootstrap(NetworkTypes.TEST_NET);
+
+const transactionHttp = new TransactionHttp({domain: "104.128.226.60"});
+
+// Replace with a cosignatory private key
+const privateKey: string = process.env.PRIVATE_KEY;
+
+const account = Account.createWithPrivateKey(privateKey);
+
+const transferTransaction = TransferTransaction.create(
+    TimeWindow.createWithDeadline(),
+    new Address("TCFFOM-Q2SBX7-7E2FZC-3VX43Z-TRV4ZN-TXTCGW-BM5J"),
+    XEM(2),
+    EmptyMessage
+);
+
+const signedTransaction = account.signTransaction(transferTransaction);
+
+transactionHttp.announceTransaction(signedTransaction).subscribe( x => console.log(x));
