b27f73bc036521f90ace10cb82b449b91bc73607
Aleix Morgadas <aleix602@gmail.com>
2017-07-18 11:16:10 +0200

    CREATED How_to_get_the_unconfirmed_transactions_for_an_account.ts

---
+++ howto/account/How_to_get_the_unconfirmed_transactions_for_an_account.ts
@@ -0,0 +1,10 @@
+import {AccountHttp, Address, NEMLibrary, NetworkTypes} from "nem-library";
+
+NEMLibrary.bootstrap(NetworkTypes.TEST_NET);
+
+const accountHttp = new AccountHttp();
+
+accountHttp.unconfirmedTransactions(new Address("TCFFOM-Q2SBX7-7E2FZC-3VX43Z-TRV4ZN-TXTCGW-BM5J"))
+    .subscribe(x => {
+        console.log(x);
+    });
