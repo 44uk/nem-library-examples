b2fb0447453f532dbee3d9f0ab740adc9d38ee1b
Aleix Morgadas <aleix602@gmail.com>
2017-07-18 11:43:30 +0200

    ADDED ChainHttpExample.ts

---
+++ concepts/blockchain/ChainHttpExample.ts
@@ -0,0 +1,9 @@
+import {ChainHttp, NEMLibrary, NetworkTypes} from "nem-library";
+
+// Inicializate NEMLibrary for TEST_NET Network
+NEMLibrary.bootstrap(NetworkTypes.TEST_NET);
+
+const chainHttp = new ChainHttp({domain: "104.128.226.60"});
+chainHttp.getBlockchainLastBlock().subscribe(block => {
+    console.log(block);
+});
