c585ad65c932738af2a23c77f90fdbb080bbe7c6
Guillem <guillemsolecubilo@gmail.com>
2017-08-13 18:37:50 +0200

    ADDED correct implementation AccountOwnedMosaicsService.ts

--- concepts/service/AccountOwnedMosaicsService.ts
+++ concepts/service/AccountOwnedMosaicsService.ts
@@ -1,15 +1,10 @@
-/*
- * Copyright 2017 Aleix Morgadas
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
- */
+import {NEMLibrary, NetworkTypes, Address, AccountOwnedMosaicsService, AccountHttp, MosaicHttp} from "nem-library";
+
+// Initialize NEMLibrary for TEST_NET Network
+NEMLibrary.bootstrap(NetworkTypes.TEST_NET);
+
+let address = new Address("");
+let accountOwnedMosaics = new AccountOwnedMosaicsService(new AccountHttp(), new MosaicHttp());
+accountOwnedMosaics.fromAddress(address).subscribe(mosaics => {
+    console.log(mosaics);
+});
