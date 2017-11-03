3f91467312a9e5d436beb50eafd8b393dc8f0513
Aleix602 <aleix602@gmail.com>
2017-11-03 15:56:30 +0100

    FIX How_to_fetch_all_transactions_for_an_account.ts

--- howto/account/How_to_fetch_all_transactions_for_an_account.ts
+++ howto/account/How_to_fetch_all_transactions_for_an_account.ts
@@ -4,7 +4,9 @@ NEMLibrary.bootstrap(NetworkTypes.TEST_NET);
 
 const accountHttp = new AccountHttp();
 const address = new Address("TCFFOM-Q2SBX7-7E2FZC-3VX43Z-TRV4ZN-TXTCGW-BM5J");
-let pagedTransactions = accountHttp.allTransactionsPaginated(address, 100);
+let pagedTransactions = accountHttp.allTransactionsPaginated(address, {
+    pageSize: 100
+});
 let time = 0;
 pagedTransactions.subscribe(x => {
     console.log("TIME ", ++time);
